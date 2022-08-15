7ddbf8f78a74fa11748ed114e316a324244647c1
ccaperna <caperna30@hotmail.com>
2022-08-15 16:09:25 +0200

    created server folder + setup

---
+++ basic_server/target/debug/basic_server.d
@@ -0,0 +1,1 @@
+/home/claudia/rust/rust-course/basic_server/target/debug/basic_server: /home/claudia/rust/rust-course/basic_server/src/main.rs
---
+++ basic_server/target/debug/deps/basic_server-b190ba073c470697.d
@@ -0,0 +1,5 @@
+/home/claudia/rust/rust-course/basic_server/target/debug/deps/basic_server-b190ba073c470697: src/main.rs
+
+/home/claudia/rust/rust-course/basic_server/target/debug/deps/basic_server-b190ba073c470697.d: src/main.rs
+
+src/main.rs:
