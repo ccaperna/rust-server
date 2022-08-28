3c0e9494bc3a7a0bd1e44e49b62e24df0971ea09
ccaperna <105978815+ccaperna@users.noreply.github.com>
2022-08-28 15:03:51 +0200

    Delete basic_server/target directory

--- basic_server/target/debug/deps/basic_server-269d5951e5fdb1b0.d
+++
@@ -1,9 +0,0 @@
-/home/ccaperna/rust/rust-course/basic_server/target/debug/deps/basic_server-269d5951e5fdb1b0.rmeta: src/main.rs src/server.rs src/http/mod.rs src/http/request.rs src/http/method.rs
-
-/home/ccaperna/rust/rust-course/basic_server/target/debug/deps/basic_server-269d5951e5fdb1b0.d: src/main.rs src/server.rs src/http/mod.rs src/http/request.rs src/http/method.rs
-
-src/main.rs:
-src/server.rs:
-src/http/mod.rs:
-src/http/request.rs:
-src/http/method.rs:
