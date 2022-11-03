92ac37e133b6eb4e0d72ab6e7e0ef27de4a31099
ccaperna <ccaperna@minsait.com>
2022-11-03 16:52:44 +0100

    added response struct

--- basic_server/src/http/mod.rs
+++ basic_server/src/http/mod.rs
@@ -1,8 +1,13 @@
+
+//select needed structs from modules
 pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
+//select modules
 pub mod request;
 pub mod method;
 pub mod query_string;
+pub mod response;
